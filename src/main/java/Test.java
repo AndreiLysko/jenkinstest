4d4e87caf31208e6590133fc943a92886227037f
AndreiLysko <lyskaman@gmail.com>
2016-12-20 14:23:39 +0300

    test

---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        int k = count(2,2);
+    }
+    public static int count(int a, int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/TestTest.java
@@ -0,0 +1,8 @@
+import org.testng.Assert;
+
+public class TestTest {
+    @org.testng.annotations.Test
+    public void TestTest(){
+        Assert.assertEquals(Test.count(2,2),4);
+    }
+}
